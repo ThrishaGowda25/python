24a31843c264826aad832dfda7a6bedcf1404b96
ThrishaGowda25 <n.thrisha07@gmail.com>
2026-02-10 22:44:43 +0530

    input

--- demo.py
+++ demo.py
@@ -7,3 +7,11 @@ print(len(word1))
 #reads a single line of input
 word3=input()
 print(word3)
+#string repetation
+print((word+" ")*3)
+#indexing print first n last letter 
+word4=input()
+first=word4[0]
+last=word4[(len(word)-1)]
+print(first)
+print(last)
