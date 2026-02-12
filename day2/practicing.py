19b809aff42071ca2cd715726a9c46a9332d2090
ThrishaGowda25 <n.thrisha07@gmail.com>
2026-02-12 18:31:51 +0530

    problems

---
+++ day2/practicing.py
@@ -0,0 +1,7 @@
+# given a word W and an integer N,write a program to print the character present at the index N in the word W
+# INPUT: Chocolate
+#      :  2
+w=str(input())
+n=int(input())
+res=w[n]
+print(res)
