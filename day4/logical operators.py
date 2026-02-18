b13f5630bac2ff9f2e96ec24b947e5309c6c3110
ThrishaGowda25 <n.thrisha07@gmail.com>
2026-02-19 00:04:09 +0530

    problems solved

---
+++ day4/logical operators.py
@@ -0,0 +1,43 @@
+a=int(input())
+res=a>25 and a<75
+print(res)
+
+a=input()
+b=input()
+c=input()
+res=(a=="woman") or (b=="woman") or (c=="woman")
+print(res)
+
+"""a=int(input())
+b=int(input())
+c=int(input())
+res=(a<15 or b<15 or c<15)
+print(res)"""
+
+
+"""a=int(input())
+b=int(input())
+res= a>0 and b>0
+print(res)"""
+
+"""a=int(input())
+b=int(input())
+res=a>=15 or b>=15
+print(res)"""
+
+
+"""a=int(input())
+b=input()
+res=(a>12 and a<60) or (b=="yes")
+print(res)"""
+
+"""a=int(input())
+b=int(input())
+res=(a+b<10) or (a-b<10) or(a>5 and a<30)
+print(res)"""
+
+"""m=int(input())
+p=int(input())
+c=int(input())
+res=(m>=70 and p>=60 and c>=60) or (m+p+c>=180)
+print(res) """
