a7a57134e308b1f2e44538a8d5db699d805834ec
ThrishaGowda25 <n.thrisha07@gmail.com>
2026-02-26 00:56:43 +0530

    practice

--- day5/condtional.py
+++ day5/condtional.py
@@ -31,3 +31,12 @@ if word<30:
     print("Ground Floor")
 else:
     print("Not Ground Floor")
+
+a=int(input())
+b=int(input())
+c=int(input())
+res=(a-b)<25 and (b-c)<25 and (c-a)<25
+if res:
+    print("Difference is less than 25")
+else:
+    print("Difference is not less than 25")
