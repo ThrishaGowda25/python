167b2c0beee44c423ad3446be7f34190bf726296
ThrishaGowda25 <n.thrisha07@gmail.com>
2026-02-26 00:54:36 +0530

    practice

--- day5/condtional.py
+++ day5/condtional.py
@@ -3,3 +3,14 @@ if n>10:
     print(n+5)
 if n<=10:
     print(n+1)
+
+a=int(input())
+b=int(input())
+c=int(input())
+sum=(a+b+c)==180
+if sum:
+    print("*")
+    print("**")
+    print("***")
+else:
+    print("Not a Valid Triangle")
