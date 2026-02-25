b67be83edabfc3b740d67035500976917fecca51
ThrishaGowda25 <n.thrisha07@gmail.com>
2026-02-26 00:55:21 +0530

    practice

--- day5/condtional.py
+++ day5/condtional.py
@@ -14,3 +14,13 @@ if sum:
     print("***")
 else:
     print("Not a Valid Triangle")
+
+
+a=int(input())
+b=int(input())
+c=int(input())
+res=a>100 and b>100 and c>100
+if res:
+    print("All are greater than 100")
+else:
+    print("Not all are greater than 100") 
